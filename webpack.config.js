3077459c94f881ffbc08ee93051ec552213a978f
lilbitsnpieces <alreadywrote@gmail.com>
2023-03-05 09:12:25 -0800

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {                 // new line
+    contentBase: './dist'      // new line
+  },
+  devtool: 'eval-source-map',  // new line
+  plugins: [
+    new ESLintWebpackPlugin(),
+    new CleanWebpackPlugin(), // new line
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
